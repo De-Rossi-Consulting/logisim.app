fa5eae66b7163b5cb14fd75fc983d16dec3de289
Ethan <ethan@drs.software>
2025-04-10 16:59:03 +0100

    feat: add warning when user closes down the tab

--- src/javascript/closeAlert.js
+++ src/javascript/closeAlert.js
@@ -1,3 +1,8 @@
 // Add an alert when the user tries to close the page to warn them that their changes might not be saved 
 
 const cheerpContainer = document.getElementById("cheerp-container");
+
+window.addEventListener("beforeunload", (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+})
