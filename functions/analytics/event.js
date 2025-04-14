9f75e504f9188333cd33ef2ba0133f29edfb42d4
Martin Nyaga <martin@martinnyaga.com>
2025-04-14 12:03:41 +0100

    Configure plausible

---
+++ functions/analytics/event.js
@@ -0,0 +1,6 @@
+export default async function onRequest(context) {
+  const request = context.request;
+  const requestClone = new Request(request);
+  requestClone.headers.delete('cookie');
+  return fetch('https://plausible.drs.software/api/event', requestClone);
+}
---
+++ functions/analytics/script.js
@@ -0,0 +1,11 @@
+export default async function onRequest(context) {
+  const request = context.request;
+  let response = await caches.default.match(request);
+  if (!response) {
+    response = await fetch(
+      "https://plausible.drs.software/js/script.js",
+    );
+    ctx.waitUntil(caches.default.put(request, response.clone()));
+  }
+  return response;
+}
