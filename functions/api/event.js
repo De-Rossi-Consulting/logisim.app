e6c0fd19948527cd6833ba71f297d884d09a3d10
Martin Nyaga <martin@martinnyaga.com>
2025-04-17 12:46:59 +0100

    fix: Update proxy request headers

--- functions/api/event.js
+++ functions/api/event.js
@@ -2,5 +2,7 @@ export async function onRequest(context) {
   const request = context.request;
   const requestClone = new Request(request);
   requestClone.headers.delete('cookie');
+  requestClone.headers.set('X-Forwarded-For', request.headers.get('CF-Connecting-IP'));
+  console.log(requestClone.headers);
   return fetch('https://plausible.drs.software/api/event', requestClone);
 }
