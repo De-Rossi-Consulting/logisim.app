8dc6a461798da354e43b91d852bc2ad3d795bb37
Martin Nyaga <martin@martinnyaga.com>
2025-04-17 16:24:11 +0100

    Removed proxy functions

--- functions/api/event.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  const request = context.request;
-  const requestClone = new Request(request);
-  requestClone.headers.delete('cookie');
-  requestClone.headers.set('X-Forwarded-For', request.headers.get('CF-Connecting-IP'));
-  return await fetch('https://plausible.drs.software/api/event', requestClone);
-}
