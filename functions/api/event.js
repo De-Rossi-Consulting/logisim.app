cd0ee0554cc11e8c7b3292b9061a1713c879e2e8
Martin Nyaga <martin@martinnyaga.com>
2025-04-17 13:01:47 +0100

    fix: Update proxy with waitUntil and fix missing await

--- functions/api/event.js
+++ functions/api/event.js
@@ -3,6 +3,5 @@ export async function onRequest(context) {
   const requestClone = new Request(request);
   requestClone.headers.delete('cookie');
   requestClone.headers.set('X-Forwarded-For', request.headers.get('CF-Connecting-IP'));
-  console.log(requestClone.headers);
-  return fetch('https://plausible.drs.software/api/event', requestClone);
+  return await fetch('https://plausible.drs.software/api/event', requestClone);
 }
--- functions/api/script.js
+++ functions/api/script.js
@@ -1,11 +1,10 @@
-export async function onRequest(context) {
-  const request = context.request;
+export async function onRequest({ request, waitUntil }) {
   let response = await caches.default.match(request);
   if (!response) {
     response = await fetch(
       "https://plausible.drs.software/js/script.js",
     );
-    await caches.default.put(request, response.clone());
+    waitUntil(caches.default.put(request, response.clone()));
   }
   return response;
 }
